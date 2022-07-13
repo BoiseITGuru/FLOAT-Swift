31afe45fe5ca114b513cdf72a4ef1c8f0fe289f8
BoiseITGuru <dev@boiseitguru.com>
2022-07-13 17:21:38 -0600

    iOS Project Creation

---
+++ FLOATs/FLOATsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLOATsApp.swift
+//  FLOATs
+//
+//  Created by Brian Pistone on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct FLOATsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
