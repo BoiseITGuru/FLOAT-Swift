eecf8ba223a7a6253df9ae4b9ec6b3db71bc7329
BoiseITGuru <dev@boiseitguru.com>
2022-08-28 17:36:37 -0600

    created IPFSImage View

--- FLOAT/Views/Groups/GroupCardView.swift
+++ FLOAT/Views/Groups/GroupCardView.swift
@@ -14,14 +14,7 @@ struct GroupCardView: View {
     var body: some View {
         NavigationLink(destination: GroupDetailView(group: group)) {
             HStack {
-                AsyncImage(url: URL(string: "https://ipfs.infura.io/ipfs/\(group.image)"), scale: 2) { image in
-                    image
-                      .resizable()
-                      .aspectRatio(contentMode: .fill)
-                } placeholder: {
-                    ProgressView()
-                        .progressViewStyle(.circular)
-                }
+                IPFSImage(cid: group.image)
                     .frame(width: 50, height: 50)
                 Text(group.name)
             }
